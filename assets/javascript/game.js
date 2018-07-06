6c2bfaf5a345a855779b0367218c34ddb1cf835b
alex-bowen <alexbowen2017@gmail.com>
2018-07-05 23:06:03 -0500

    Fixed issue with totalScore where count was resetting late

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,26 @@
 $(document).ready(function () {
     console.log("ready!");
 
-
+    // beginning stats
     var totalScore = 0;
     var wins = 0;
     var losses = 0;
+
+    // number to aim toward
     var randomGoal;
 
+    // gem values
     var crystalValue1;
     var crystalValue2;
     var crystalValue3;
     var crystalValue4;
 
-    // reset function 
+    
+
+    // reset function -- resets stats and generates random numbers all around
     function resetGame() {
         totalScore = 0;
+        $("#total-score").html(totalScore);
         randomGoal = getRandomInt(19, 120);
         $("#goalNumber").text(randomGoal);
         crystalValue1 = getRandomInt(1, 12);
@@ -29,9 +35,7 @@ $(document).ready(function () {
 
     }
 
-
-    // Generate random number for user to try to guess/add up to with buttons.
-
+    // Generates random number for user to try to guess/add up to with buttons.
     function getRandomInt(min, max) {
         min = Math.ceil(min);
         max = Math.floor(max);
@@ -41,8 +45,8 @@ $(document).ready(function () {
     randomGoal = getRandomInt(19, 120);
     $("#goalNumber").text(randomGoal);
 
-    // Generate random numbers and assign them to gems.
 
+    // Generates random numbers and assign them to gems
     crystalValue1 = getRandomInt(1, 12);
     crystalValue2 = getRandomInt(1, 12);
     crystalValue3 = getRandomInt(1, 12);
@@ -54,31 +58,40 @@ $(document).ready(function () {
     console.log("Crystal four: " + crystalValue4);
 
 
+    // Keeps track of wins and losses
     function tally() {
         if (totalScore === randomGoal) {
             wins++;
+
             console.log("Wins: " + wins);
             console.log("Losses: " + losses);
             // change html
+            $("#wins-tally").text("Wins: " + wins);
+            $("#exclamation").html("You win!");
 
-            // add reset function
             resetGame();
+
+
+
 
         } else if (totalScore > randomGoal) {
             losses++;
             console.log("Wins: " + wins);
             console.log("Losses: " + losses);
             // change html
+            $("#losses-tally").text("Losses: " + losses);
+            $("#exclamation").text("You lose!");
 
-            // add reset function 
             resetGame();
-            // $("#goalNumber").text(randomGoal);
+            
 
         }
     }
 
-    // Push value of crystals to total score when clicked, 
 
+
+
+    // Pushes value of crystals to total score when clicked, 
     $("#gem1").click(function () {
         $("#total-score").html(totalScore + crystalValue1);
         // Adds value of clicked gem to total score
@@ -107,8 +120,7 @@ $(document).ready(function () {
 
     })
 
-    // Restarts, generates random numbers all around 
-
+   
 
 });
 
