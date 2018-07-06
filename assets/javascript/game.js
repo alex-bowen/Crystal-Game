75b21c1b7176a57a15a90c7841571e0db053b909
alex-bowen <alexbowen2017@gmail.com>
2018-07-05 20:59:41 -0500

    cleaned up files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
-// $(document).ready(function () {
-//     console.log("ready!");
+$(document).ready(function () {
+    console.log("ready!");
 
 
     var totalScore = 0;
@@ -11,6 +11,24 @@
     var crystalValue2;
     var crystalValue3;
     var crystalValue4;
+
+    // reset function 
+    function resetGame() {
+        totalScore = 0;
+        randomGoal = getRandomInt(19, 120);
+        $("#goalNumber").text(randomGoal);
+        crystalValue1 = getRandomInt(1, 12);
+        crystalValue2 = getRandomInt(1, 12);
+        crystalValue3 = getRandomInt(1, 12);
+        crystalValue4 = getRandomInt(1, 12);
+
+        console.log("Crystal one: " + crystalValue1);
+        console.log("Crystal two: " + crystalValue2);
+        console.log("Crystal three: " + crystalValue3);
+        console.log("Crystal four: " + crystalValue4);
+
+    }
+
 
     // Generate random number for user to try to guess/add up to with buttons.
 
@@ -37,13 +55,28 @@
 
 
     function tally() {
-    if (totalScore === randomGoal) {
-        wins++;
-    } else if (totalScore > randomGoal) {
-        losses++;
+        if (totalScore === randomGoal) {
+            wins++;
+            console.log("Wins: " + wins);
+            console.log("Losses: " + losses);
+            // change html
+
+            // add reset function
+            resetGame();
+
+        } else if (totalScore > randomGoal) {
+            losses++;
+            console.log("Wins: " + wins);
+            console.log("Losses: " + losses);
+            // change html
+
+            // add reset function 
+            resetGame();
+            // $("#goalNumber").text(randomGoal);
+
+        }
     }
-    }
-    
+
     // Push value of crystals to total score when clicked, 
 
     $("#gem1").click(function () {
@@ -74,17 +107,9 @@
 
     })
 
+    // Restarts, generates random numbers all around 
 
 
-
-
-    // Restarts, generates random numbers all around
-
-
-    console.log("Wins: " + wins);
-    console.log("Losses: " + losses);
-
-
-// });
+});
 
 
