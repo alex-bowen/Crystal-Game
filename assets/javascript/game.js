eed6964111c76edf05251415a96c3a89fc5272c4
alex-bowen <alexbowen2017@gmail.com>
2018-07-06 09:35:55 -0500

    Cleaned up code + added README

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,5 @@
+// Thanks for looking! 
+
 $(document).ready(function () {
     console.log("ready!");
 
@@ -16,7 +18,6 @@ $(document).ready(function () {
     var crystalValue4;
 
     
-
     // reset function -- resets stats and generates random numbers all around
     function resetGame() {
         totalScore = 0;
@@ -62,7 +63,6 @@ $(document).ready(function () {
     function tally() {
         if (totalScore === randomGoal) {
             wins++;
-
             console.log("Wins: " + wins);
             console.log("Losses: " + losses);
             // change html
@@ -87,8 +87,6 @@ $(document).ready(function () {
 
         }
     }
-
-
 
 
     // Pushes value of crystals to total score when clicked, 
@@ -119,8 +117,6 @@ $(document).ready(function () {
         tally();
 
     })
-
-   
 
 });
 
